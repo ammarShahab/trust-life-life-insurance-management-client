a6ecf5072257c37aa6d6eb052ae620b4cde68dfa
ammarShahab <ashahab007@gmail.com>
2026-06-18 20:48:10 +0600

    refactor: remove unused SweetAlert initialization code from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import Swal from "sweetalert2";
 import App from "./App.jsx";
-
-// Initialize Sweet Alert
-Swal.fire({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Toaster position="top-left" />
-  </StrictMode>
+  </StrictMode>,
 );
